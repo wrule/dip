95719680b13aefdf4d2fd209dd8fc62867557f3e
jimao <jimao@jimaodeMacBook-Pro.local>
2022-10-15 23:05:48 +0800

    客户端基本完成

--- client.ts
+++ client.ts
@@ -1,2 +1,19 @@
+import axios from 'axios';
+import moment from 'moment';
 
-console.log('客户端');
+let timer: any = null;
+
+async function main() {
+  try {
+    clearTimeout(timer);
+    console.log(`${moment(new Date()).format('YYYY-MM-DD HH:mm:ss')} >> 请求`);
+    await axios.post(`http://xxxx:xxxx/ip`);
+  } catch (e) {
+    console.log(e);
+  } finally {
+    console.log(`${moment(new Date()).format('YYYY-MM-DD HH:mm:ss')} >> 响应`);
+    timer = setTimeout(main, 10000);
+  }
+}
+
+main();
