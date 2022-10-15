05807188a0699f6054d2c936b5bc3a9f099e3caf
jimao <jimao@jimaodeMBP.lan>
2022-10-15 21:27:45 +0800

    提交代码

--- server.ts
+++ server.ts
@@ -1,15 +1,25 @@
 import Koa from 'koa';
 import Router from 'koa-router';
+import moment from 'moment';
+import fs from 'fs';
 
 const app = new Koa();
 const router = new Router();
+
+const file_name = 'ip.txt';
 
 router.post('/ip', (ctx) => {
   ctx.body = 'fine';
 });
 
 router.get('/ip', (ctx) => {
-  ctx.body = 'some ip';
+  try {
+    ctx.body = fs.readFileSync(file_name, 'utf-8');
+  } catch (e) {
+    console.error(e);
+  } finally {
+    ctx.body = '暂无上报';
+  }
 });
 
 app
